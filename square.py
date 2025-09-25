da79a22ca41276cbc79c5e3b60485fcacfda33e1
Mona Irsfeld <mona.irsfeld@arcor.de>
2025-09-25 09:37:18 +0200

    add docstring comments

--- square.py
+++ square.py
@@ -1,9 +1,20 @@
 def square(n):
+    """
+    Simple function to square a number
+
+    Args:
+        n (float): number to square
+
+    Returns:
+        float: square of n
+    """
     return n*n
 
 if __name__=='__main__':
+
     try:
         n = float(input("Enter number to square:"))
         print(square(n))
+        
     except ValueError:
         print("Please enter a valid number")
