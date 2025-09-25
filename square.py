f4ad6c64c14d834b14dcf72561d19ea3194b7b5a
Mona Irsfeld <mona.irsfeld@arcor.de>
2025-09-25 10:11:37 +0200

    add main function

--- square.py
+++ square.py
@@ -10,11 +10,13 @@ def square(n):
     """
     return n*n
 
-if __name__=='__main__':
-
+def main():
     try:
         n = float(input("Enter number to square:"))
         print(square(n))
-        
+
     except ValueError:
         print("Please enter a valid number")
+
+if __name__=='__main__':
+    main()
